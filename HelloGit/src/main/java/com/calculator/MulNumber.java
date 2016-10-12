a7e8cd5076602067d2912daf1592adcd6c51098d
Norbert Fabian <norbert_fabian@epam.com>
2016-10-12 16:21:07 +0200

    cherry mul

--- HelloGit/src/main/java/com/calculator/MulNumber.java
+++ HelloGit/src/main/java/com/calculator/MulNumber.java
@@ -5,7 +5,7 @@ import com.util.Calculate;
 public class MulNumber implements Calculate{
 
 	public int calculate(int a, int b) {
-		System.out.println("MUL");
+		System.out.println("MUL value");
 		return a*b;
 	}
 
